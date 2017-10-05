02e81773ec3e66f173608070ff99b736218cde64
WANDEE SREPENRAK <59050300@kmitl.ac.th>
2017-10-05 23:44:54 +0700

    Create Repository

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Game from './Game'
+
+export class App extends Component {
+    render() {
+        return (
+           <Game/>
+        );
+    }
+}
